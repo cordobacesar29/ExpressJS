a551152fc967858773e9b8587415d3111b114979
César Córdoba <cordobacesar29@gmail.com>
2021-05-11 20:42:41 -0300

    post and get methot with express.js

--- index.js
+++ index.js
@@ -1,36 +1,33 @@
 const express = require('express');
-const { crear } = require('./data-handler');
+const { crear, listar } = require('./data-handler');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 const port = 5000;
 
+app.use(express.json());
+app.use(express.urlencoded());
+
 app.get('/', (_req, res) => {
     res.send('la API está corriendo exitósamente');
 })
-app.get('/profile', (_req, res) => {
-    const userProfile = [
-        {
-            firstName:'Céser', 
-            lastName:'Córdoba', 
-            email:'cordobacesar29@gmail.com ',
-            password:'test123',
-        }
-    ]
+app.get('/profile', async (_req, res) => {
+    const userProfile = await listar({directorioEntidad:'profile'});
     res.status(200).json(userProfile);
 });
 
-app.post('/profile', async (_req, res) =>{
-    const newProfile = await crear({
-        directorioEntidad:'profile',
-        nombreArchivo:'profile1',
-        datosGuardar:{
-            firstName:'Céser', 
-            lastName:'Córdoba', 
-            email:'cordobacesar29@gmail.com ',
-            password:'',
-        },
-    });
-    res.status(200).json(newProfile);
+app.post('/profile', async (req, res) =>{
+    if( req.body && Object.keys(req.body).length > 0 ){
+        const _id = uuidv4();
+        const dateNewProfile = {...req.body, _id};
+        const newProfile = await crear({
+            directorioEntidad:'profile',
+            nombreArchivo: _id,
+            datosGuardar: dateNewProfile,
+        });
+        return res.status(200).json(newProfile);
+    }
+    return res.status(400).json({mensaje: "falta el body"});   
 });
   
 app.listen(port, () => {
