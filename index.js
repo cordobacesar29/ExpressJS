ab82df45ee8f10fe25b2d2373db5b5a24e93ad83
César Córdoba <cordobacesar29@gmail.com>
2021-05-10 22:47:18 -0300

    GET /profile with express

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
-import express from 'express';
+const express = require('express');
 
 const app = express();
-const port = 3000;
+const port = 5000;
+
+app.get('/', (_req, res) => {
+    res.send('la API está corriendo exitósamente');
+})
+app.get('/profile', (_req, res) => {
+    const userProfile = [
+        {
+            firstName:'Céser', 
+            lastName:'Córdoba', 
+            email:'cordobacesar29@gmail.com ',
+            password:'',
+        }
+    ]
+    res.status(200).json(
+
+    );
+    res.send('la API está corriendo exitósamente');
+})
+  
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
