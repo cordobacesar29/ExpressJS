856375a395768216e89fe4b7dc08141862033a39
César Córdoba <cordobacesar29@gmail.com>
2021-05-12 23:30:10 -0300

    get single profile

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const express = require('express');
-const { crear, listar } = require('./data-handler');
+const { crear, 
+    listar, 
+    actualizar, 
+    eliminar,
+    obtenerUno,
+} = require('./data-handler');
 const { v4: uuidv4 } = require('uuid');
 
 const app = express();
@@ -10,11 +15,27 @@ app.use(express.urlencoded());
 
 app.get('/', (_req, res) => {
     res.send('la API está corriendo exitósamente');
-})
-app.get('/profile', async (_req, res) => {
-    const userProfile = await listar({directorioEntidad:'profile'});
-    res.status(200).json(userProfile);
 });
+
+app.get('/profile', async (_req, res) => {
+    const usersProfile = await listar({directorioEntidad:'profile'});
+    res.status(200).json(usersProfile);
+});
+
+app.get("/profile/:_id", async (req, res) => {
+    const { _id = null } = req.params;
+    if (!_id) {
+      return res.status(400).json({ mensaje: "Falta el id" });
+    }
+    const singleProfile = await obtenerUno({
+      directorioEntidad: "profile",
+      nombreArchivo: _id,
+    });
+    if (singleProfile) {
+      res.status(200).json(singleProfile);
+    }
+    res.status(404).json({ mensaje: "no encontrado" });
+  });  
 
 app.post('/profile', async (req, res) =>{
     if( req.body && Object.keys(req.body).length > 0 ){
@@ -29,6 +50,36 @@ app.post('/profile', async (req, res) =>{
     }
     return res.status(400).json({mensaje: "falta el body"});   
 });
+
+app.put('/profile/:_id', async (req, res) => {
+    const{_id= null} = req.params;
+
+    if(!_id) {
+        return res.status(400).json({mensaje:'falta el id'});
+    }
+    if( req.body && Object.keys(req.body).length > 0 ){
+        const currentDate = {...req.body, _id};
+        const profileUpdate = await actualizar({
+            directorioEntidad:'profile', 
+            nombreArchivo: _id,
+            datosActuales: currentDate,
+        })
+        return res.status(200).json(profileUpdate);
+    }
+    return res.status(400).json({mensaje: "falta el body"});   
+});
+
+app.delete('/profile/:_id', async ( req, res ) => {
+    const { _id = null } = req.params;
+    if(!_id) {
+        return res.status(400).json({mensaje: 'falta el id'});
+    }
+    await eliminar({
+        directorioEntidad:'profile',
+        nombreArchivo: _id,
+    });
+    return res.status(204).send();
+})
   
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
